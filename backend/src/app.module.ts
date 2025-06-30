a38103e3ddc5167cb70fb813a42d5d7527cae388
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-06-30 22:48:20 +0200

    chore(backend): configure app module and main entry point

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,35 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { cwd } from "node:process";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from './users/users.module';
+import { User } from "./users/entity/user.entity";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    ignoreEnvFile: false,
-    cache: true,
-    envFilePath: join(cwd(), '..', '.env.development'),
-  })],
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        ignoreEnvFile: false,
+        cache: true,
+        envFilePath: join(cwd(), '..', '.env.development'),
+      }),
+      TypeOrmModule.forRootAsync({
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          type: 'postgres',
+          host: config.get<string>('DB_HOST', 'localhost'),
+          port: config.get<number>('DB_PORT', 5432),
+          username: config.get<string>('DB_USER', 'postgres'),
+          password: config.get<string>('DB_PASS', 'postgres'),
+          database: config.get<string>('DB_NAME', 'nostalgiamapdb'),
+          entities: [User],
+          synchronize: true,
+        }),
+      }),
+      UsersModule,
+      AuthModule
+  ],
 })
 export class AppModule {}
