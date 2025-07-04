51993df2e1912165d7b0f588831e1823c9263ed2
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-07-04 12:52:18 +0200

    chore(users): implement update, delete and get methods with permission check and validation

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable} from '@nestjs/common';
+import {ConflictException, ForbiddenException, Injectable, Logger, NotFoundException} from '@nestjs/common';
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import * as bcrypt from 'bcrypt';
@@ -6,10 +6,12 @@ import { plainToInstance } from "class-transformer";
 import { User } from "./entity/user.entity";
 import { UserDto } from "./dto/user.dto";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
+    private readonly logger = new Logger(UsersService.name, { timestamp: true });
 
     async create(createUserDto: CreateUserDto): Promise<UserDto> {
         const existingUser = await this.usersRepository.findOne({
@@ -38,5 +40,45 @@ export class UsersService {
 
     async findById(id: number): Promise<User | null> {
         return this.usersRepository.findOne({ where: { id } });
+    }
+
+    async update(updateUserDto: UpdateUserDto, userId: number, requestId: number): Promise<UserDto> {
+        const user: User | null = await this.usersRepository.findOne({ where: { id: userId } });
+        if (!user) {
+            throw new NotFoundException("User not found");
+        }
+
+        if (+requestId !== +userId) {
+            throw new ForbiddenException("You don't have permission");
+        }
+
+        Object.assign(user, updateUserDto);
+        await this.usersRepository.save(user);
+
+        this.logger.log(`Updating user of id: ${userId}`);
+        return plainToInstance(UserDto, user, {
+            excludeExtraneousValues: true,
+        });
+    }
+
+    async delete(requestId: number, userId: number): Promise<void> {
+        if (+requestId !== +userId) {
+            throw new ForbiddenException("You don't have permission");
+        }
+
+        this.logger.log(`Deleting user of id: ${userId}`);
+        await this.usersRepository.delete(userId);
+    }
+
+    async getUser(userId: number): Promise<UserDto> {
+        const user = await this.usersRepository.findOne({ where: { id: userId }, relations: ['memories'] });
+        if (!user) {
+            throw new NotFoundException("User not found");
+        }
+
+        this.logger.log(`Getting user of id: ${userId}`);
+        return plainToInstance(UserDto, user, {
+            excludeExtraneousValues: true,
+        });
     }
 }
