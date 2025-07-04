3286c988add4981c1a1c8219b38241680b031c7d
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-07-04 02:34:10 +0200

    chore(user): add forwardRef to resolve circular dependency with MemoriesModule

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersService } from './users.service';
 import { User } from "./entity/user.entity";
+import { UsersController } from './users.controller';
+import { MemoriesModule } from "../memories/memories.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+      forwardRef(() => MemoriesModule),
+      TypeOrmModule.forFeature([User])
+  ],
   providers: [UsersService],
+  controllers: [UsersController],
   exports: [UsersService],
 })
 export class UsersModule {}
