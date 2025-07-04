f8a74446949e73aee56e114220813e9531d765ff
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-07-04 02:22:47 +0200

    chore(auth): add JWT strategy and auth guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService} from "@nestjs/config";
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from "../users/users.module";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
@@ -19,7 +20,7 @@ import { UsersModule } from "../users/users.module";
         })
       })
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
