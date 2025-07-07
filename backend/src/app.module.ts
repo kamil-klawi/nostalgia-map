dfec100a2599ded09b5c398044b9647698c18100
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-07-07 16:54:37 +0200

    feat(auth): add verification code entity and mail module to configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,12 +9,14 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MemoriesModule } from './memories/memories.module';
 import { CategoriesModule } from './categories/categories.module';
+import { MailModule } from './mail/mail.module';
 
 // Entities
 import { User } from "./users/entity/user.entity";
 import { Memory } from "./memories/entity/memory.entity";
 import { Category } from "./categories/entity/category.entity";
 import { Reaction } from "./memories/entity/reaction.entity";
+import { VerificationCode } from "./auth/entity/verification-code.entity";
 
 // Services
 import { AppService } from './app.service';
@@ -36,7 +38,7 @@ import { AppService } from './app.service';
           username: config.get<string>('DB_USER', 'postgres'),
           password: config.get<string>('DB_PASS', 'postgres'),
           database: config.get<string>('DB_NAME', 'nostalgiamapdb'),
-          entities: [User, Memory, Category, Reaction],
+          entities: [User, Memory, Category, Reaction, VerificationCode],
           synchronize: true,
         }),
       }),
@@ -44,6 +46,7 @@ import { AppService } from './app.service';
       AuthModule,
       MemoriesModule,
       CategoriesModule,
+      MailModule,
   ],
   providers: [AppService],
 })
