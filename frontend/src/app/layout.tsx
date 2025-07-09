2ff05ebe41a64ce9313b5324bc1dc222c52306b4
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-07-09 20:07:39 +0200

    feat(app): add Toaster

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../styles/base/_index.scss";
+import { Toaster } from "sonner";
+import "../styles/styles.scss";
+import "../styles/main.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster position={"top-center"} richColors />
       </body>
     </html>
   );
