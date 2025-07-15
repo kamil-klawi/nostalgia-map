fd6ecac58447045ceae047c6858286b3859643aa
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-07-15 15:45:22 +0200

    chore: configure domains for images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    allowedDevOrigins: ['http://localhost:3001/', 'http://192.168.178.46:3001'],
+  allowedDevOrigins: ['http://localhost:3001/', 'http://192.168.178.46:3001'],
+  images: {
+    domains: ['images.pexels.com'],
+  },
 };
 
 export default nextConfig;
